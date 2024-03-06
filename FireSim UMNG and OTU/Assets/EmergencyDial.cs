0e77f1da761026da438fe80d5c1d0378a2e70af6
mark-Toufic <72156370+mark-Toufic@users.noreply.github.com>
2024-03-06 15:07:59 -0500

    Phone Functionality and UI

---
+++ FireSim UMNG and OTU/Assets/EmergencyDial.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using TMPro;
+
+public class EmergencyDialer : MonoBehaviour
+{
+    public TMP_Text inputText; 
+    public GameObject respondentsUI;
+    private string dialedNumbers = "";
+
+    public void OnNumberButtonClicked(int number)
+    {
+        if (dialedNumbers.Length < 3)
+        {
+            dialedNumbers += number.ToString();
+            UpdateNumberDisplay(dialedNumbers);
+        }
+
+        if (dialedNumbers.Length == 3)
+        {
+            if (dialedNumbers == "911")
+            {
+                ShowRespondentsOnTheWay();
+            }
+            else
+            {
+                ResetDialer();
+            }
+        }
+    }
+
+    private void ResetDialer()
+    {
+        dialedNumbers = "";
+        UpdateNumberDisplay(dialedNumbers);
+    }
+
+    private void ShowRespondentsOnTheWay()
+    {
+        respondentsUI.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+
+    private void UpdateNumberDisplay(string numbers)
+    {
+        inputText.text = numbers;
+    }
+}
